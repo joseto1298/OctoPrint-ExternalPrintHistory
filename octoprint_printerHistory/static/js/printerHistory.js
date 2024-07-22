db6a2f3cf7a9d805b86c21646be2df07ac37a4a9
joseto1298 <jtomas1298@gmail.com>
2024-07-22 21:49:50 +0200

    HTML OK

--- octoprint_printerHistory/static/js/printerHistory.js
+++ octoprint_printerHistory/static/js/printerHistory.js
@@ -1,12 +1,18 @@
-// printerHistory.js
-
 $(function () {
     function PrinterHistoryViewModel(parameters) {
         var self = this;
         self.settingsViewModel = parameters[0];
-        self.popup = undefined;
 
-        self.onDataUpdaterPluginMessage = function (plugin, data) {};
+        // Maneja mensajes del plugin de datos
+        self.onDataUpdaterPluginMessage = function (plugin, data) {
+            if (plugin !== "PrinterHistory") {
+                return;
+            }
+
+            // Procesar datos específicos del plugin
+            console.log("Mensaje del plugin de historial de impresoras:", data);
+            // Implementar lógica adicional aquí
+        };
     }
 
     OCTOPRINT_VIEWMODELS.push({
